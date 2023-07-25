ee00ca498c25b961829182e8b281385eb04b7976
hanwenbo <han.wenbo@nets-tech.com>
2023-07-25 17:21:36 +0800

    init

---
+++ src/main/java/org/j2c/Jacoco2Cobertura.java
@@ -0,0 +1,52 @@
+package org.j2c;
+
+import java.io.IOException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
+
+import org.j2c.core.XMLMain;
+import org.j2c.utils.Validator;
+import org.xml.sax.SAXException;
+
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+
+@Command(name = "Jacoco2Cobertura", mixinStandardHelpOptions = true, version = "Jacoco2Cobertura 1.0.0", description = "Convert Jacoco xml file to Cobertura xml.")
+public class Jacoco2Cobertura implements Runnable {
+
+    @Option(required = true, names = {"-j", "--jacoco"}, description = "Path to the jacoco xml file.", arity = "1..*")
+    private String jacocoFile;
+
+    @Option(names = {"-d", "--destination"}, description = "path to save Cobertura xml file.", defaultValue = ".")
+    private String destination;
+
+    @Option(required = false, names = {"-s", "--source"}, description = "Path to java source root. If program have more than one src directory pass all with comma seperated", arity = "1..*", split = ",", defaultValue = "src/main/java")
+    private String[] source_roots;
+
+    public Jacoco2Cobertura() {
+    }
+
+    public static void main(String[] args) {
+    	
+    	//args = "-j target/mjacoco.xml -s src/main/java -d target/coverage.xml".split(" ");
+
+        new CommandLine(new Jacoco2Cobertura()).execute(args);
+    }
+
+    @Override
+    public void run() {
+        if (Validator.validateJacocoFilePath(jacocoFile)) {
+            System.out.println("XML file Validated");
+            XMLMain xmlMain = new XMLMain(jacocoFile, destination, source_roots);
+            try {
+                xmlMain.init();
+                xmlMain.createCoberturaCoverageRootTag();
+            } catch (TransformerException | ParserConfigurationException | IOException | SAXException e) {
+                System.out.println("Some Error Occurred. Please try again if problem persist please raise an issue in Github");
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
